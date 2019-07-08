65f204ce0bcce91c248ef8d2acef34826c6488ad
yangkejia66 <yangkejia66@foxmail.com>
2019-07-08 16:10:09 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <AppHeader></AppHeader>
+    <div class="container-fluid">
+      <div class="row">
+        <AppRightNav></AppRightNav>
+        <AppHerosList></AppHerosList>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from "./components/AppHeader";
+import AppRightNav from "./components/AppRightNav";
+import AppHerosList from "./components/AppHerosList";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    AppHeader,
+    AppRightNav,
+    AppHerosList
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>

93f94d11e6d60a173d80ef3ecda2694e427a6845
yangkejia66 <yangkejia66@foxmail.com>
2019-07-08 16:28:11 +0800

    2

--- src/main.js
+++ src/main.js
@@ -2,8 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import "../node_modules/bootstrap/dist/css/bootstrap.css"
 import "../src/assets/css/index.css"
+import VueRouter from "../node_modules/vue-router/dist/vue-router"
 Vue.config.productionTip = false
+Vue.use(VueRouter)
 
+const appRouter = new VueRouter({
+  routes:[
+
+  ]
+})
 new Vue({
   render: h => h(App),
+  router:appRouter
 }).$mount('#app')
